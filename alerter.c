bab6a61a3560dc56e9ff4299d41db1cd11e056a5
Rihan Malik <rihannb@yahoo.in>
2022-06-28 12:07:42 +0530

    Seprating network alert stub code from production

--- alerter.c
+++ alerter.c
@@ -1,20 +1,8 @@
 #include <stdio.h>
 #include <assert.h>
+#include "stubNwAlert.h"
 
 int alertFailureCount = 0;
-
-int networkAlertStub(float celcius) {
-    printf("ALERT: Temperature is %.1f celcius.\n", celcius);
-
-    if (celcius > 200)
-    {
-        return 500;
-    }
-    // Return 200 for ok
-    // Return 500 for not-ok
-    // stub always succeeds and returns 200
-    return 200;
-}
 
 void alertInCelcius(float farenheit) {
     float celcius = (farenheit - 32) * 5 / 9;
